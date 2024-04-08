16b2c04e198b66b714c6d4a3ea05677580af16bb
sdcavallin <sdcavallin@gmail.com>
2024-04-08 15:09:49 -0400

    Added unit tests for 100% line coverage

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, fireEvent, screen } from "@testing-library/react";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counter = screen.getByTestId("count");
+  expect(counter).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counter = screen.getByTestId("count");
+  const incrementButton = screen.getByText("+");
+
+  fireEvent.click(incrementButton);
+
+  expect(counter).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counter = screen.getByTestId("count");
+  const decrementButton = screen.getByText("-");
+
+  fireEvent.click(decrementButton);
+
+  expect(counter).toHaveTextContent("-1");
 });
+ 
